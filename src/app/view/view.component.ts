aa028459a352fb14948d846db1f88fe39be6dc78
nitdevv <nitesh@excellencetechnologies.in>
2017-10-17 14:58:42 +0530

    inplement to edit task

--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewService } from './view.service';
 import { RegisterService } from '../service/register.service';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { ModalModule } from 'ng2-modal'
+
+
 
 
 
@@ -13,15 +17,26 @@ import { RegisterService } from '../service/register.service';
 export class ViewComponent {
   data: any;
   datas: any;
+  editData: any;
+  editTitleForm: FormGroup;
 
 
   constructor(
     private _viewService: ViewService,
-    private registerService: RegisterService
+    private registerService: RegisterService,
+    private _formBuilder: FormBuilder
   ) {
   }
   ngOnInit() {
     this.view();
+
+    this._viewService.view().subscribe(data => {
+      this.data = data.data
+    })
+    this.editTitleForm = this._formBuilder.group({
+      title: [null, [Validators.required]]
+    })
+
   }
   view() {
     this._viewService.view().subscribe(data => {
@@ -35,5 +50,17 @@ export class ViewComponent {
     });
 
   }
+  editTitle(id: any) {
+    this._viewService.view().subscribe(data => { console.log(this.editData = data) })
+
+
+  }
+  // changeTitle(id: any, task: any) {
+  //   this.registerService.edittask(id, task).subscribe((data) => { })
+  //   this._viewService.view().subscribe((data) => {
+  //     this.data = data.data
+  //   })
+  // }
+
 
 }
